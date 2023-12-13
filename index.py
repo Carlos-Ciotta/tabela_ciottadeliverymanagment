fbf140dab1698822f23c9362b1af61d09e30b852
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2023-12-13 18:12:22 -0300

    a

--- index.py
+++ index.py
@@ -20,11 +20,46 @@ def populate_table():
     # Exibir a tabela
     st.table(df)
 
+def insere_entrega():
+    nome_cliente = st.text_input("Nome cliente", key="nome_cliente_key")
+    rua = st.text_input("Rua", key="rua_key")
+    bairro = st.text_input("Bairro", key="bairro_key")
+    telefone = st.text_input("Telefone", key="telefone_key")
+
+    if st.button("Enviar Entrega"):
+        data = {
+            "nome_cliente": nome_cliente,
+            "logradouro": rua,
+            "bairro": bairro,
+            "telefone": telefone,
+            "id": 0,
+            "status": "Aguardando",
+            "hora": "NULL",
+            "data": "NULL"
+        }
+        url = 'https://api-production-e20e.up.railway.app/entregas/post'
+        response = requests.post(url, json=data)
+        nome_cliente = ""
+        rua = ""
+        bairro = ""
+        telefone = ""
+        if response.status_code == 200:
+            data = response.json()
+            print("Dados da API:", data)
+        else:
+            print("Falha na solicitação. Código de status:", response.status_code)
+            print("Conteúdo da resposta:", response.text)
+
+        st.write(f"Resposta do servidor: {response.text}")
+    
 def main():
     st.title("Gestão de Entregas - Ciotta")
+    with st.container():
+        insere_entrega()
 
     with st.container():
         populate_table()
-
+        if st.button("Atualizar"):
+            populate_table()
 if __name__ == "__main__":
     main()
