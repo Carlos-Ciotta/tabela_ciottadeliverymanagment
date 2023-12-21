9dd64d40ba5b154812deb2d66300ba1e467783a7
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2023-12-21 17:24:52 -0300

    commit

--- index.py
+++ index.py
@@ -6,11 +6,31 @@ from PIL import Image
 icon = Image.open('Logo.jpeg')
 var_status = ['Em andamento', 'Aguardando']
 var_status1 = ['Entregue']
+
 st.set_page_config(
     page_title="Ciotta - Delivery Managment",
     layout="wide",
     page_icon=icon,
 )
+
+def verifica_repetidos(dados_novos):
+    dados = get_all()
+    colunas = ["ID", "Nome Cliente", "Rua", "Bairro", "Telefone", "Status", "Hora", "Data", "Previsão de Entrega"]
+    data = pd.DataFrame(dados, columns=colunas)
+    df = data[data['Status'].isin(var_status)]
+
+    colunas_desejadas = ["Nome Cliente", "Rua", "Bairro"]
+    df_temp = df[colunas_desejadas]
+
+    indices_desejados = list(dados_novos.keys())[:3]
+    dados = {k: dados_novos[k] for k in indices_desejados}
+    # Cria uma condição que verifica se pelo menos uma linha atende à condição
+    condicao = (df_temp == dados).all(axis=1)
+
+    if condicao.any():
+        return True
+    else:
+        return False
 
 def populate_table(var):
     dados = get_all()
@@ -42,8 +62,13 @@ def insere_entrega():
                 "data": "NULL",
                 "previsao":previsao
             }
-            response = post(data)
-            return response
+            aux = verifica_repetidos(data)
+            if aux:
+                response = post(data)
+                return response
+            else:
+                return st.write("Entrega já enviada")
+                
         else:
             return st.write("Campos de texto inválidos")
     
